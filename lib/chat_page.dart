43f1496e09134146348b440fe2a645303a49ee07
Lev Berlinkov <lev.b@greeninvoice.co.il>
2024-09-17 17:32:34 +0300

    update headers

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -53,6 +53,7 @@ class _ChatPageState extends State<ChatPage> {
       final response = await http.post(
         Uri.parse(apiUrl),
         headers: {
+          'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
           'Access-Control-Allow-Origin': '*',
           "Access-Control-Allow-Methods": "POST, GET, OPTIONS, PUT, DELETE, HEAD",
           'Content-Type': 'application/json',
